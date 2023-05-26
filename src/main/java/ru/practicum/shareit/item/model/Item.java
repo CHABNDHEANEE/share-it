3b5d5f5c89533ad8dde8fe0ace43a73c9de92f77
Daniil <rusbear060@gmail.com>
2023-05-26 17:39:14 +0300

    create entity

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,20 +2,38 @@ package ru.practicum.shareit.item.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import ru.practicum.shareit.user.model.User;
 
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@Entity
+@RequiredArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @NotNull
+    @Column
     private String name;
+
     @NotNull
+    @Column
     private String description;
+
     @NotNull
+    @Column
     private boolean available;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column
     private User owner;
+
+    @Column
     private Long request;
 }
