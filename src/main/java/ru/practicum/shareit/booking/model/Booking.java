20af1360f83c9ef9a939a4a08b2ca16d3fbf9783
Daniil <rusbear060@gmail.com>
2023-05-26 17:42:37 +0300

    create entity

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,20 +1,35 @@
 package ru.practicum.shareit.booking.model;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.*;
 import java.util.Date;
 
-/**
- * TODO Sprint add-bookings.
- */
+
 @Data
+@Entity
+@Table(name = "bookings", schema = "public")
+@RequiredArgsConstructor
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
     private Item item;
+
+    @Column(name = "start_date")
     private Date start;
+
+    @Column(name = "end_date")
     private Date end;
+
+    @OneToOne
     private User booker;
+
+    @Enumerated(EnumType.STRING)
     private BookingStatus status;
 }
