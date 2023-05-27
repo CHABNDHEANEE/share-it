f8b3d2c758d1a3a2d8cbbe80ab7a3539c906b494
Daniil <rusbear060@gmail.com>
2023-05-27 04:57:19 +0300

    add BookingDto

--- src/main/java/ru/practicum/shareit/booking/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/BookingDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.user.User;
+
+import java.util.Date;
+
+@Data
+@Builder
 public class BookingDto {
+    private long id;
+    private Item item;
+    private Date start;
+    private Date end;
+    private User booker;
+    private BookingStatus status;
 }
